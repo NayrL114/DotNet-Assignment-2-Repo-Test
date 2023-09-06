e486d261834202851e8402d7d4f4fabf8005fb72
NayrL <40843838+NayrL114@users.noreply.github.com>
2023-09-06 18:00:04 +1000

    initial file commit

---
+++ Assignment_2_Test/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace TestProgram
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Hello World");
+
+
+            Console.ReadKey();
+        }
+    }
+
+    public abstract class Person
+    {
+        //public int Id { get; set; }
+        public string Name { get; set; }
+        //public string Description { get; set; }
+        public Person(string personName)
+        {   
+            this.Name = personName;
+        }
+
+    }
+
+    public class Patient: Person
+    {
+        private int Patient_ID;
+
+        public Patient(int patient_ID): base(Name)
+        {
+            this.Patient_ID = patient_ID;
+        }
+    }
+}
